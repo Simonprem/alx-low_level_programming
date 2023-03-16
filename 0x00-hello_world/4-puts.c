0579efbb601f6cddf37859d86f0d1963a1da80a8
Simon prem <jenfimohamed@gmail.com>
2023-03-16 14:27:21 +0100

    TASK 5

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - is the main
+ * Return: we have always to write it so the programme can finish
+ */
+
+int main(void)
+{
+printf("\"Programming is like building a multilingual puzzle\n");
+return (0);
+}
