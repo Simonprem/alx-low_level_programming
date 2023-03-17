b4a9eacd1048ec5184be65012626de763ad916cb
Simonprem <jenfimohamed@gmail.com>
2023-03-17 05:28:45 -0700

    TASK 1

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -20,7 +20,7 @@ printf("is %d and is greater than 5" , n % 10);
 }
 else if (n % 10 < 6 && n == 0)
 {
-printf(is %d and is less than 6 and not 0" , n % 10);
+printf("is %d and is less than 6 and not 0" , n % 10);
 }
 else
 {
