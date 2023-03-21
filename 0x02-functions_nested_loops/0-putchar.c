4351f1560864e62ac04d46011830a3819b1b121c
Simonprem <jenfimohamed@gmail.com>
2023-03-21 05:56:10 -0700

    TASK 0 _putchar function

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,4 +1,4 @@
-#include "_putchar"
+#include "main.h"
 /**
  * main - is the main function
  * Description: we will inclyse new functions
@@ -6,14 +6,15 @@
  */
 int main(void)
 {
-	_putchar("_");
-	_putchar("p");
-	_putchar("u");
-	_putchar("t");
-	_putchar("c");
-	_putchar("h");
-	_putchar("a");
-	_putchar("r");
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
 	return (0);
 }
 
