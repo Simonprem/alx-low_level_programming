12f568d3277d3a220b8914dbf71d31d164c57394
Simonprem <jenfimohamed@gmail.com>
2023-03-21 07:11:41 -0700

    TASK1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * main - is the main function
+ * Description: we will inclyse new functions
+ * Return: 0 is success
+ */
+void print_alphabet(void)
+{
+
+	int n;
+
+	for (n = 'a'; n <= 'z'; n++)
+	{
+		_putchar(n);
+	}
+	_putchar('\n');
+}
