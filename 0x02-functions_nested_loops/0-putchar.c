21e2d72197743e88b077fac79ed7b8c213582760
Simonprem <jenfimohamed@gmail.com>
2023-03-21 05:38:56 -0700

    TASK 0 _putchar function

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include "_putchar"
+/**
+ * main - is the main function
+ * Description: we will inclyse new functions
+ * Return: 0 is success
+ */
+int main(void)
+{
+	_putchar("_");
+	_putchar("p");
+	_putchar("u");
+	_putchar("t");
+	_putchar("c");
+	_putchar("h");
+	_putchar("a");
+	_putchar("r");
+	return (0);
+}
+
