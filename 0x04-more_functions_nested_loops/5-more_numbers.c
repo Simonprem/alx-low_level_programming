52fab982e9412ee962fd7b5b254c23b4dc6de010
Simonprem <jenfimohamed@gmail.com>
2023-03-23 08:18:20 -0700

    Hot fix 5 characters

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,22 +1,21 @@
 #include "main.h"
+
 /**
- * more_numbers - duplicate 0 to 9 14 times
- * description: We'll print numbers from 0 to 9.
- * Return: as usualy 0 is sucess so let's try it !
+ * more_numbers - Prints the numbers 0 to 14, ten times
  */
 void more_numbers(void)
 {
-	int e;
-	int t;
+    int i, j;
 
-	for (e = 0; e < 10; e++)
-	{
-		for (t = 0; t <= 14; t++)
-		{
-			if (t >= 10)
-				_putchar('1');
-				_putchar(t % 10 + '0');
-		}
-			_putchar('\n');
-	}
+    for (i = 0; i < 10; i++)
+    {
+        for (j = 0; j <= 14; j++)
+        {
+            if (j >= 10)
+                _putchar('1');
+            _putchar(j % 10 + '0');
+        }
+        _putchar('\n');
+    }
 }
+
