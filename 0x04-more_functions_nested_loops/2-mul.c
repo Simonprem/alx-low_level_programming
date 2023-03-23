6a7e0af54049a58b25b066e53503c53cdfd6d4de
Simonprem <jenfimohamed@gmail.com>
2023-03-23 05:05:30 -0700

    TASK 2 This time i will succed

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * mul - is the multiplication
+ * @a: the first int
+ * @b: the second int
+ * Return: like usual but this time return will be to multiplicator
+ */
+int mul(int a, int b)
+{
+
+	return (a * b);
+}
