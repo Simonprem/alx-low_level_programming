92f971ccfb018daa4fd4bba1bf8b936849cc2b9b
Simonprem <jenfimohamed@gmail.com>
2023-04-27 15:49:24 -0700

    My fourth com

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -3,25 +3,26 @@
 #include "lists.h"
 
 /**
- * _strlen -  The string length
- * @str: The string 
- * Return: e
+ * _strlen - finds the length of a string
+ * @str: string to find the length of
+ *
+ * Return: length of string
  */
 unsigned int _strlen(char *str)
 {
-	unsigned int e;
+	unsigned int i;
 
-	for (e = 0; str[e]; e++)
-		
-	return (e);
+	for (i = 0; str[i]; i++)
+		;
+	return (i);
 }
 
 /**
  * add_node_end - adds a new node to the end of linked list
- * @head: is a double pointer to a linked list
- * @str: string 
+ * @head: double pointer to a linked list
+ * @str: string to add to the new node
  *
- * Return: New
+ * Return: pointer to the new node
  */
 list_t *add_node_end(list_t **head, const char *str)
 {
