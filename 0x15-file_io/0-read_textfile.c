974770fbc8ce99753c04009a1d0d319d1e9ec0ba
Simonprem <jenfimohamed@gmail.com>
2023-05-12 15:47:35 -0700

    TASK 0

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -2,35 +2,28 @@
 #include <stdlib.h>
 
 /**
- * read_textfile - Create a function that creates a file.
- * @filename: A name of the file.
- * @letters: The number of letters to print.
- * Return: NULL or Z
+ * read_textfile- Read text file print to STDOUT.
+ * @filename: text file being read
+ * @letters: number of letters to be read
+ * Return: w- actual number of bytes read and printed
+ *        0 when function fails or filename is NULL.
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	ssize_t o, r, w;
-	char *fer;
+	char *buf;
+	ssize_t fd;
+	ssize_t w;
+	ssize_t t;
 
-	if (filename == NULL)
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
 		return (0);
+	buf = malloc(sizeof(char) * letters);
+	t = read(fd, buf, letters);
+	w = write(STDOUT_FILENO, buf, t);
 
-	fer = malloc(sizeof(char) * letters);
-	if (fer == NULL)
-		return (0);
-
-	o = open(filename, O_RDONLY);
-	r = read(o, fer, letters);
-	w = write(STDOUT_FILENO, fer, r);
-
-	if (o == -1 || r == -1 || w == -1 || w != r)
-	{
-		free(fer);
-		return (0);
-	}
-
-	free(fer);
-	close(o);
-
+	free(buf);
+	close(fd);
 	return (w);
 }
+
